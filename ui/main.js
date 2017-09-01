eb01764686817600dd0dd324c80ec8b0ac3b181e
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2017-09-01 21:47:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,5 @@
 console.log('Loaded!');
 var imag = document.getElementById("img");
 img.onclick = function(){
-    img.style.marginleft = "100px";
-    
-};
+    img.style.marginleft = '100p';
+    };
