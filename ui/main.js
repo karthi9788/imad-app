e325379b39d3b089765f124c1f6872c11b03065b
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2017-09-02 10:50:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,7 @@ madi.onclick = function () {
 var button = document.getElementById("counter");
 var counter = 0;
  button.onclick = function (){
+     counter = counter + 1;
      var span =document.getElementById("count");
      span.innerHtml = counter.toString();
      
